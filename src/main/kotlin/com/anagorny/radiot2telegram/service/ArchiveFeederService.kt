5e98c4647ae6c860a1df34ccd3cd9e8f3c622d34
nagorny <mail@anagorny.com>
2019-08-24 18:00:19 +0300

    Archive RSS slicing for chunks

--- src/main/kotlin/com/anagorny/radiot2telegram/service/ArchiveFeederService.kt
+++ src/main/kotlin/com/anagorny/radiot2telegram/service/ArchiveFeederService.kt
@@ -23,6 +23,9 @@ class ArchiveFeederService : AbstractFeederService() {
     @Value("\${radio_t_feeder.archive.rss.url}")
     private lateinit var archiveRssUrl: String
 
+    @Value("\${radio_t_feeder.archive.batch_size}")
+    private var batchSize: Int = 5
+
     private val locker = ReentrantLock()
 
     fun archiveProcessing() {
@@ -34,46 +37,55 @@ class ArchiveFeederService : AbstractFeederService() {
             val entries = getNotSyncedEntries()
             val total = entries.size
             logger.info("RSS feed received with $total new entries")
-            val futuresWithDownloaded = ConcurrentHashMap<Int, Future<FeedItemWithFile>>()
 
-            for ((index, entry) in entries.withIndex()) {
-                val future: Future<FeedItemWithFile> = asyncPreparingFile(entry, total, index + 1)
-                futuresWithDownloaded.putIfAbsent(index, future)
-            }
+            val groupedEntries = entries.asSequence()
+                    .mapIndexed { index, value -> (index to value) }
+                    .chunked(Math.max(batchSize, 1))
+                    .toList()
 
+            for ((groupNum, group) in groupedEntries.withIndex()) {
+                val futuresWithDownloaded = ConcurrentHashMap<Int, Future<FeedItemWithFile>>()
 
-            for (offset in futuresWithDownloaded.keys().asSequence().sorted()) {
-                val future = futuresWithDownloaded.getValue(offset)
-                val feedWithFile = future.get()
-                val feed = feedWithFile.item
-
-                lateinit var file: File
-                try {
-                    file = File(feedWithFile.filePath)
-                } catch (e: Exception) {
-                    logger.error("Cant read file '${feedWithFile.filePath}' fo sending", e)
-                    throw e
+                val groupStartDate = System.currentTimeMillis()
+                for ((index, entry) in group) {
+                    val future: Future<FeedItemWithFile> = asyncPreparingFile(entry, total, index + 1)
+                    futuresWithDownloaded.putIfAbsent(index, future)
                 }
+                for (offset in futuresWithDownloaded.keys().asSequence().sorted()) {
+                    val future = futuresWithDownloaded.getValue(offset)
+                    val feedWithFile = future.get()
+                    val feed = feedWithFile.item
 
-                try {
-                    val message = telegramBot.sendAudio(file, feed)
-                    if (message == null) {
-                        logger.error("Message '${feed.title}' cant sended to Telegram (response is null!)")
-                        metaInfoContainer.appendToEnd(feed)
-                    } else {
-                        logger.info("Message '${feed.title}' successfully sended to Telegram")
-                        feed.tgMessageId = message.messageId
-                        feed.tgFileId = message.audio.fileId
-                        metaInfoContainer.appendToEnd(feed)
+                    lateinit var file: File
+                    try {
+                        file = File(feedWithFile.filePath)
+                    } catch (e: Exception) {
+                        logger.error("Cant read file '${feedWithFile.filePath}' fo sending", e)
+                        throw e
                     }
-                    logger.info("Message '${feed.title}' successfully processed")
-                } catch (e: Exception) {
-                    throw e
-                } finally {
-                    removeFile(file, logger)
+
+                    try {
+                        val message = telegramBot.sendAudio(file, feed)
+                        if (message == null) {
+                            logger.error("Message '${feed.title}' cant sended to Telegram (response is null!)")
+                            metaInfoContainer.appendToEnd(feed)
+                        } else {
+                            logger.info("Message '${feed.title}' successfully sended to Telegram")
+                            feed.tgMessageId = message.messageId
+                            feed.tgFileId = message.audio.fileId
+                            metaInfoContainer.appendToEnd(feed)
+                        }
+                        logger.info("Message '${feed.title}' successfully processed")
+                    } catch (e: Exception) {
+                        throw e
+                    } finally {
+                        removeFile(file, logger)
+                    }
                 }
+                logger.info("Group ${groupNum + 1}/${groupedEntries.size} has been processed on ${(System.currentTimeMillis() - groupStartDate) / 1000 / 60.0} minutes")
             }
-            logger.info("Archive RSS has been processed on ${System.currentTimeMillis() - startDate} ms")
+
+            logger.info("Archive RSS has been processed on ${(System.currentTimeMillis() - startDate) / 1000 / 60.0} minutes")
         } finally {
             locker.unlock()
         }
